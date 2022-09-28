f8d0289967866c2ec258020b690ba871641bb6c6
Riya Mishra <rmishra50.be18@gmail.com>
2022-09-28 22:22:05 +0530

    CodingChallenge1st

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
-const country = "India";
-const continent = "Asia";
-let population = 1380000000;
-const isIsland  = false;
-let language;
-console.log(country+" "+ continent+" "+ population+" "+ isIsland+" "+language);
+//First Coding Challenge(udemy)
+const weightMarkInKg = 95;
+const heightMarkInMetre = 1.88;
 
-language = 'Hindi';
-console.log(continent);
-let halfPopulation = population/2;
-console.log(halfPopulation);
-population+=1;
-console.log(population);
-let populationFinland = 6000000
-let isPopulationHigh = (population>populationFinland);
-console.log(isPopulationHigh);
-let averagePopulation = 33000000;
-let isPopulationLess=(averagePopulation>population);
-console.log(isPopulationLess);
+const weightJohnInKg = 85;
+const heightJohnInMetre = 1.76;
 
-const description = country+' is in '+continent+', and its 1.38 million people speak '+language;
-console.log(description); 
+const MarkBMI = weightMarkInKg / heightMarkInMetre ** 2;
+const JohnBMI = weightJohnInKg / heightJohnInMetre ** 2;
+
+const markHigherBMI = (MarkBMI>JohnBMI);
+console.log(markHigherBMI);
