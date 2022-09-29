234f348a043c3e2f12e3fbb6643efa255eb611bc
Riya Mishra <rmishra50.be18@gmail.com>
2022-09-29 08:44:26 +0530

    StringLiteralsAndConditions

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-//First Coding Challenge(udemy)
-const weightMarkInKg = 95;
-const heightMarkInMetre = 1.88;
+const country = "India";
+const continent = "Asia";
+let population = 1380000000;
+const isIsland  = false;
+let language;
+console.log(country+" "+ continent+" "+ population+" "+ isIsland+" "+language);
 
-const weightJohnInKg = 85;
-const heightJohnInMetre = 1.76;
+language = 'Hindi';
+console.log(continent);
+let halfPopulation = population/2;
+console.log(halfPopulation);
+population+=1;
+console.log(population);
+let populationFinland = 6000000
+let isPopulationHigh = (population>populationFinland);
+console.log(isPopulationHigh);
+let averagePopulation = 33000000;
+let isPopulationLess=(averagePopulation>population);
+console.log(isPopulationLess);
 
-const MarkBMI = weightMarkInKg / heightMarkInMetre ** 2;
-const JohnBMI = weightJohnInKg / heightJohnInMetre ** 2;
+const description = `${country} is in ${continent}, and its ${population} people speak ${language}`
+console.log(description); 
 
-const markHigherBMI = (MarkBMI>JohnBMI);
-console.log(markHigherBMI);
+if(population>averagePopulation){
+    console.log(`${country}'s population is above average`);
+}else{
+    console.log(`${country}'s population is ${averagePopulation-population} below average`);
+}
