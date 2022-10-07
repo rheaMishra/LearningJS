6e2521c0b36eab9d6904458f3f7a8f331c61fead
Riya Mishra <rmishra50.be18@gmail.com>
2022-10-07 11:24:38 +0530

    coding challenge #4

--- script.js
+++ script.js
@@ -74,22 +74,110 @@
 
 
 //3rd coding challenge
-let minimumScore = 100;
-// let dolphinsAverageScore = (97+112+101)/3;
-let dolphinsAverageScore = (96+108+89)/3;
+// let minimumScore = 100;
+// // let dolphinsAverageScore = (97+112+101)/3;
+// let dolphinsAverageScore = (96+108+89)/3;
 
-let koalasAverageScore = (96+108+89)/3;
-// let koalasAverageScore = (109+95+123)/3;
-console.log(`Dolphins Score = ${dolphinsAverageScore}
-Koalas Score = ${koalasAverageScore}`)
-if(dolphinsAverageScore > koalasAverageScore && dolphinsAverageScore >= minimumScore){
-    console.log(`Dolphins are winner `);
-}else if (koalasAverageScore > dolphinsAverageScore && koalasAverageScore >=minimumScore ){
-    console.log(`Koalas are winner`);
-}else{
-    if(dolphinsAverageScore === koalasAverageScore  && dolphinsAverageScore >= minimumScore && koalasAverageScore >= min){
-    console.log(`Both win the trophy`);
-    }else{
-        console.log(`No one wins the trophy`);
-    }
-}
+// let koalasAverageScore = (96+108+89)/3;
+// // let koalasAverageScore = (109+95+123)/3;
+// console.log(`Dolphins Score = ${dolphinsAverageScore}
+// Koalas Score = ${koalasAverageScore}`)
+// if(dolphinsAverageScore > koalasAverageScore && dolphinsAverageScore >= minimumScore){
+//     console.log(`Dolphins are winner `);
+// }else if (koalasAverageScore > dolphinsAverageScore && koalasAverageScore >=minimumScore ){
+//     console.log(`Koalas are winner`);
+// }else{
+//     if(dolphinsAverageScore === koalasAverageScore  && dolphinsAverageScore >= minimumScore && koalasAverageScore >= min){
+//     console.log(`Both win the trophy`);
+//     }else{
+//         console.log(`No one wins the trophy`);
+//     }
+// }
+
+//switch statement
+
+// let day = '';
+// switch(day){
+// case 'Monday':
+// console.log('It is monday')
+// break;
+// case 'Tuesday':
+// console.log('It is tuesday')
+// break;
+// case 'Wednesday':
+// console.log('It is wednesday')
+// break;
+// case 'Thursday':
+// console.log('It is thursday');
+// break;
+// case 'Friday':
+// case 'Saturday':
+// console.log('It is either friday or saturday');
+// break;
+// case 'Sunday':
+// console.log('It is sunday');
+// default:
+//     console.log('Invalid');
+// }
+// let day = 'Monday';
+// if(day === 'Monday'){
+//     console.log('It is monday')
+// }else if(day === 'Tuesday'){
+//     console.log('It is tuesday')
+// }else if(day === 'Wednesday'){
+//     console.log('It is wednesday')
+// }else if(day === 'Thursday'){
+//     console.log('It is thursday');
+// }else if(day === 'Friday'||day ==='Saturday'){
+//     console.log('It is either friday or saturday');
+// }else if(day === 'Sunday'){
+//     console.log('It is sunday');
+// }else{
+//     console.log('Invalid');
+// }
+
+
+// let language = 'cinese';
+// switch(language){
+//     case 'chinese':
+//     case 'mandarian':
+//         console.log('MOST number of native speakers!');
+//         break;
+//     case 'spanish':
+//         console.log('2nd place in number of native speakers');
+//         break;
+//     case 'english':
+//         console.log('3rd place');
+//         break;
+//     case 'hindi':
+//         console.log('Number 4');
+//         break;
+//     case 'arabic':
+//         console.log('5th most spoken language');
+//         break;
+//     default:
+//         console.log('Great language too :D');
+// }
+
+
+//Ternary operator
+// let population = 320000000;
+// population>330000000?console.log(`Portugal's population is above average`):console.log(`Portugal's population is below average`);
+
+
+
+//Coding challenge $4
+// let billValue = 40;
+
+// if(billValue >=50 && billValue<=300){
+//     let tip = (15/100)*billValue;
+//     let finalValue = tip+billValue;
+//     console.log(`The bill was ${billValue}, the tip was ${tip}, and the total value ${finalValue}`);
+// }else{
+//     let tip = (15/100)*billValue;
+//     let finalValue = tip+billValue;
+//     console.log(`The bill was ${billValue}, the tip was ${tip}, and the total value ${finalValue}`);
+// }
+const billValue = 275;
+const tip = billValue >=50 && billValue<=300 ?  billValue*0.15 : billValue*0.2;
+console.log(`The bill was ${billValue}, the tip was ${tip}, and the total value ${billValue+tip}`);
